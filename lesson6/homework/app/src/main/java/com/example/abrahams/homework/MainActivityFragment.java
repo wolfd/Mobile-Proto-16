3ca132f8e56ae53be78ca915c59f3ede45ef39b1
David Abrahams <david.abrahams@here.com>
2016-07-24 19:16:50 -0400

    Finished Stock homework

--- lesson6/homework/app/src/main/java/com/example/abrahams/homework/MainActivityFragment.java
+++ lesson6/homework/app/src/main/java/com/example/abrahams/homework/MainActivityFragment.java
@@ -9,10 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +34,9 @@ import butterknife.ButterKnife;
 public class MainActivityFragment extends Fragment {
 
     @BindView(R.id.editText) EditText editText;
+    @BindView(R.id.listView) ListView listView;
+
+    ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -32,6 +46,9 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
+        adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_view);
+
+        listView.setAdapter(adapter);
 
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -47,24 +64,50 @@ public class MainActivityFragment extends Fragment {
     }
 
     public void searchTwitter(String search) {
-        // Get a RequestQueue
-        RequestQueue queue = MySingleton.getInstance(this.getActivity().getApplicationContext()).
-                getRequestQueue();
+
+        adapter.clear();
+
+        String[] split = search.split(" ");
+        String stocks = "";
+
+        for (String s : split) {
+            stocks += s + ",";
+        }
 
         Uri.Builder builder = new Uri.Builder();
-        builder.scheme("https")
-                .authority("api.twitter.com/1.1/search/tweets.json")
-//                .appendPath("turtles")
-//                .appendPath("types")
-                .appendQueryParameter("q", "Donald Trump");
-//                .appendQueryParameter("sort", "relevance")
-//                .fragment("section-name");
+        builder.scheme("http")
+                .authority("finance.google.com")
+                .appendPath("finance")
+                .appendPath("info")
+                .appendQueryParameter("client", "iq")
+                .appendQueryParameter("q", stocks);
         String myUrl = builder.build().toString();
-        Log.d("my tag", myUrl);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, myUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        String sub = response.substring(3);
+                        try {
+                            JSONArray json = (JSONArray) new JSONParser().parse(sub);
+                            for (int i = 0; i < json.size(); i++) {
+                                JSONObject o = (JSONObject) json.get(i);
+                                adapter.add(o.get("t") + ": $" + o.get("l"));
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        Log.d("URL", myUrl);
 
         // Add a request (in this example, called stringRequest) to your RequestQueue.
-//        MySingleton.getInstance(this.getActivity()).addToRequestQueue(stringRequest);
-
+        MySingleton.getInstance(this.getActivity()).addToRequestQueue(stringRequest);
 
     }
 
