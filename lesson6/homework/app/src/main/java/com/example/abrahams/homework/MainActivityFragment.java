d9bbd61374dd34c4e49caf1cf208550396bf9157
David Abrahams <david.abrahams@here.com>
2016-07-24 16:16:58 -0400

    Listener works

--- lesson6/homework/app/src/main/java/com/example/abrahams/homework/MainActivityFragment.java
+++ lesson6/homework/app/src/main/java/com/example/abrahams/homework/MainActivityFragment.java
@@ -2,19 +2,23 @@ package com.example.abrahams.homework;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    @BindView(R.id.editText) EditText username;
+    @BindView(R.id.editText) EditText editText;
 
     public MainActivityFragment() {
     }
@@ -22,6 +26,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    return true;
+                }
+                return false;
+            }
+        });
+        return view;
     }
+
+    public void searchTwitter(String search) {
+        
+    }
+
 }
