48fe5f67a3ccb752ade3bb74fc681647088cdd15
wolfd <2dawolf@gmail.com>
2016-09-10 17:27:21 -0400

    Add Account, CheckingAccount, MoneySaver, and MoneyTester. Use jUnit for MoneyTester instead of a main method.

---
+++ lesson2/Lesson2Project/money/src/main/java/io/wolfd/mobileproto/Account.java
@@ -0,0 +1,33 @@
+package io.wolfd.mobileproto;
+
+public abstract class Account {
+    private long amount;
+
+    public Account(long amount) {
+        this.amount = amount;
+    }
+
+    public static Account largerAccount(Account acc1, Account acc2) {
+        if (acc1.amount > acc2.amount) {
+            return acc1;
+        } else {
+            return acc2;
+        }
+    }
+
+    public String toString() {
+        return "Account Balance: $" + amount;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public void deposit(long amount) {
+        this.amount += amount;
+    }
+}
